c898d065475dbf381d19075b0d1a7b5677933720
Mohammad Shahed <mohammadshahed5358@gmail.com>
2022-04-24 15:04:33 +0600

    delete button addeed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const objectId = require('mongodb').objectId;
 
 // use middleware
 app.use(cors());
@@ -22,13 +23,22 @@ async function run(){
   try{
     await client.connect();
     const userCollection = client.db("foodExpress").collection("user");
-    
+
+
+    // get users
     app.get('/user', async(req, res)=>{
       const query = {};
       const cursor = userCollection.find(query);
       const users = await cursor.toArray();
       res.send(users);
 
+    });
+     
+    app.get('/user/:id', async(req, res)=>{
+       const id = req.params.id;
+       const query = {_id: objectId(id)};
+       const result = await userCollection.findOne(query);
+       res.send(result);
     })
 
     // POST User: add a new user
@@ -36,8 +46,16 @@ async function run(){
       const newUser = req.body;
       console.log('adding new user', newUser);
       const result = await userCollection.insertOne(newUser);
-      res.send(result)
+      res.send(result);
     });
+
+    // delete a user
+    app.delete('/user/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: objectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
   }
   finally{
     // await client.close();
